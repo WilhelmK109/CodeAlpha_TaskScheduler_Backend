a97deb2e81196cbef5d7bf349cea9c5f00d4da0e
WilhelmK109 <kamulungaw@gmail.com>
2023-11-22 15:34:57 +0200

    Add tasks and notifications controller

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,26 @@
+class NotificationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @task = current_user.tasks.find(params[:task_id])
+    @notification = @task.notifications.build(notification_params)
+
+    if @notification.save
+      render json: @notification
+    else
+      render json: { errors: @notification.errors.full_message }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @notification = current_user.notifications.find(params[:id])
+    @notification.destroy
+    head :no-content
+  end
+
+  private
+
+  def notification_params
+    params.require(:notification).permit(:scheduled_at)
+  end
+end
